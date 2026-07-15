df7ffaff6a306549f6a24f71cfb3e94163b6797a
paiva-glm <paivazin01@gmail.com>
2026-07-15 04:57:01 -0300

    feat: criação do controller para listar tarefas

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\Lista_tarefasController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('h');
 });
+
+Route::get('/lista', [Lista_tarefasController::class, 'listar_tarefas'])->name('listar_tarefas');
